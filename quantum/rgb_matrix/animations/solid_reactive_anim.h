764307d30b20d0c81056d503e3eb59e8e2d96b49
m-bagby <101544990+m-bagby@users.noreply.github.com>
2023-03-12 13:36:23 -0400

    custom reactive effects

--- quantum/rgb_matrix/animations/solid_reactive_anim.h
+++ quantum/rgb_matrix/animations/solid_reactive_anim.h
@@ -4,10 +4,57 @@ RGB_MATRIX_EFFECT(SOLID_REACTIVE)
 #        ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 static HSV SOLID_REACTIVE_math(HSV hsv, uint16_t offset) {
-#            ifdef RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
-    hsv.h = scale16by8(g_rgb_timer, add8(rgb_matrix_config.speed, 1) >> 6);
-#            endif
-    hsv.h += qsub8(130, offset);
+//     hsv.h += qsub8(130, offset);
+
+    //using saturation setting to control secondary color and then setting all color's saturation back to full
+    int primaryHue = (int)hsv.h; //Primary color is color setting
+    if (hsv.s == 0) {
+        hsv.s = 1;
+    }
+    int secondaryHue = (int)hsv.s; //Secondary color is saturation setting
+
+    int hueDifference = primaryHue - secondaryHue;
+    int hueShift;
+
+    if (hueDifference < 128 && hueDifference > -128) {
+      hueShift = - hueDifference;
+    }
+    else if (hueDifference > 0) {
+      hueShift = 255 - hueDifference;
+    }
+    else {
+      hueShift = -(255 + hueDifference);
+    }
+
+    hsv.s = 255;
+
+
+    //keys that have been pressed get offset towards the secondary color
+    //subtracting the offset channel value blueshifts the hue of secondary color
+    //I want the hueshift direction (+=, -=) to go towards the closest. (teal -> purple) (purple <- teal)
+    //To do this, I need to assign color hue and find which direction is closer..
+
+    if (hueShift > 0) {
+    hsv.h += qsub8((uint8_t)hueShift, offset);
+    }
+    else {
+    hueShift *= -1;
+    hsv.h -= qsub8((uint8_t)hueShift, offset);
+    }
+
+
+    //I want a lower background brightness with pressed keys starting at secondary color bright
+    //then dimming with their hue shift back to the background brightness
+    uint8_t brightness = (uint8_t)(hsv.v / 3); //background 1/3rd of highlight
+
+    brightness += qsub8((brightness * 2), offset);
+    hsv.v = brightness;
+
+//OLD NOTES: This was implemented
+    //It'd probably be better to have the brightness setting be the max brightness (the brightness a keypress goes to)
+    //Which would mean setting any keys that haven't been pressed recently (have no offset?)
+    //to a fraction of the brightness setting. I think 1/2 would be good
+
     return hsv;
 }
 
