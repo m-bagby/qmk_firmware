c01364d5eb98565d64327aec682d949a9191acee
m-bagby <101544990+m-bagby@users.noreply.github.com>
2023-03-12 16:18:14 -0400

    Fixed idle keyboard effect runner bug

--- quantum/rgb_matrix/animations/runners/effect_runner_reactive_splash.h
+++ quantum/rgb_matrix/animations/runners/effect_runner_reactive_splash.h
@@ -10,14 +10,18 @@ bool effect_runner_reactive_splash(uint8_t start, effect_params_t* params, react
     uint8_t count = g_last_hit_tracker.count;
     for (uint8_t i = led_min; i < led_max; i++) {
         RGB_MATRIX_TEST_LED_FLAGS();
-        HSV hsv = rgb_matrix_config.hsv;
-        //hsv.v   = 0;
+        HSV hsv;
+
+        //Set hsv to background in case tracker count is 0 and for loop, therefore effect, doesn't get run
+        hsv = rgb_matrix_config.hsv;
+        hsv.s   = 255;
+        
         for (uint8_t j = start; j < count; j++) {
             int16_t  dx   = g_led_config.point[i].x - g_last_hit_tracker.x[j];
             int16_t  dy   = g_led_config.point[i].y - g_last_hit_tracker.y[j];
             uint8_t  dist = sqrt16(dx * dx + dy * dy);
             uint16_t tick = scale16by8(g_last_hit_tracker.tick[j], qadd8(rgb_matrix_config.speed, 1));
-            hsv           = effect_func(hsv, dx, dy, dist, tick);
+            hsv           = effect_func(rgb_matrix_config.hsv, dx, dy, dist, tick);
         }
         //hsv.v   = scale8(hsv.v, rgb_matrix_config.hsv.v);
         RGB rgb = rgb_matrix_hsv_to_rgb(hsv);
